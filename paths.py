1aee097adf4ad8aba6bf0edcf5bb2be597ece5f5
APGR22 <143316176+APGR22@users.noreply.github.com>
2023-10-15 20:10:55 +0700

    not important

--- paths.py
+++ paths.py
@@ -1,11 +1,28 @@
+# Copyright © 2023 APGR22
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import platform
 
 if platform.system() == "Windows":
-    before_symbol = "/"
-    after_symbol = "\\"
-elif platform.system() == "Linux":
-    before_symbol = "\\"
-    after_symbol = "/"
+    symbol = "\\"
+else: #Linux and Darwin
+    symbol = "/"
 
+def ganti_simbol(path: str) -> str:
+    path = path.replace("\\", symbol)
+    path = path.replace("/", symbol)
+        
+    return path
 
-path = "Paths"+after_symbol
+path = "Paths"+symbol
