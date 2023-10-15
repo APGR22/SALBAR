57b5a690bde8c44544ce8217b039218d8988b545
APGR22 <143316176+APGR22@users.noreply.github.com>
2023-10-15 20:06:55 +0700

    not important

--- extension.py
+++ extension.py
@@ -1,4 +1,4 @@
-# """Copyright © 2023 APGR22
+# Copyright © 2023 APGR22
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,9 +10,9 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License."""
+# limitations under the License.
 
-import convert
+import convert #demi mudah dipahami meski performa dikorbankan
 import os
 import paths
 bahasa_1 = "insert string di salbar dengan atas nama dir1 = >>"
